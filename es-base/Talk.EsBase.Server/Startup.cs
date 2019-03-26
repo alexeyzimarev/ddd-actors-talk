054b23c183fa5040b9e9b737ef14ccbc6a0396b3
Alexey Zimarev <alex@zimarev.com>
2019-03-26 18:07:07 +0100

    Adding Prometheus

--- es-base/Talk.EsBase.Server/Startup.cs
+++ es-base/Talk.EsBase.Server/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Prometheus;
 using Talk.EsBase.Server.Services;
 
 namespace Talk.EsBase.Server
@@ -22,6 +23,9 @@ namespace Talk.EsBase.Server
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMetricServer();
+            app.UseHttpMetrics();
 
             app.UseRouting(routes => { routes.MapGrpcService<EventProcessorService>(); });
         }
